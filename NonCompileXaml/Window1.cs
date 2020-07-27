14191df18a3696b8c7a9f98ab4983cefa06b3f5b
awsti <awsti@DESKTOP-AT8CETC>
2020-07-27 14:39:54 +0800

    modify

--- NonCompileXaml/Window1.cs
+++ NonCompileXaml/Window1.cs
@@ -5,7 +5,7 @@ using System.Windows.Markup;
 
 namespace NonCompileXaml
 {
-    public partial class Window1 : Window
+    public class Window1 : Window
     {
         private Button button1;
 
@@ -16,27 +16,21 @@ namespace NonCompileXaml
 
         private void InitializeComponent(string xamlFile)
         {
-            // Configure the form.
             this.Width = this.Height = 285;
             this.Left = this.Top = 100;
             this.Title = "Dynamically Loaded XAML";
 
-            // Get the XAML content from an external file.
             DependencyObject rootElement;
             using (FileStream fs = new FileStream(xamlFile, FileMode.Open))
             {
                 rootElement = (DependencyObject)XamlReader.Load(fs);
             }
 
-            // Insert the markup into this window.
             this.Content = rootElement;
 
-            // Find the control with the appropriate name.
-            //button1 = (Button)LogicalTreeHelper.FindLogicalNode(rootElement, "button1");
             FrameworkElement frameworkElement = (FrameworkElement)rootElement;
             button1 = (Button)frameworkElement.FindName("button1");
 
-            // Wire up the event handler.
             button1.Click += new RoutedEventHandler(button1_Click);
         }
 
